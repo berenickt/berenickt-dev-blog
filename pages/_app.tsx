b87a276400e95fd1f8abb03ad7a18ead78594743
Beren <berenickt99@gmail.com>
2023-11-22 22:10:23 +0900

    ADD:SEO-Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,41 @@
-import type { ReactElement } from "react"
-import type { AppProps } from "next/app"
-import "../style.css"
-import "@code-hike/mdx/dist/index.css"
+import type { ReactElement } from 'react'
+// @ SEO 적용
+import { DefaultSeo } from 'next-seo'
 
-export default function Nextra({Component, pageProps}: AppProps): ReactElement {
-  return <Component {...pageProps} />
+// @ Vercel Analytics로 방문자 수 집계
+import { Analytics } from '@vercel/analytics/react'
+
+import type { AppProps } from 'next/app'
+import '../style.css'
+import '@code-hike/mdx/dist/index.css'
+
+const DEFAULT_SEO = {
+  title: 'berenickt',
+  description: "berenickt's Dev Blog",
+  canonical: 'https://www.carrotins.com',
+  openGraph: {
+    type: 'website',
+    locale: 'ko_KR',
+    url: 'https://berenickt.vercel.app/',
+    title: "berenickt - Yewon-Noh's Dev Blog",
+    siteName: 'berenickt',
+    images: [
+      {
+        url: '/images/image.png',
+        width: 285,
+        height: 167,
+        alt: '이미지',
+      },
+    ],
+  },
+}
+
+export default function Nextra({ Component, pageProps }: AppProps): ReactElement {
+  return (
+    <>
+      <DefaultSeo {...DEFAULT_SEO} />
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  )
 }
